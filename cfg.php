4749f5a0e448ee1b15b86630c2600f58bf8f9e44
l2okjit <80217961+l2okjit@users.noreply.github.com>
2021-03-18 13:00:42 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,10 @@
+<?php
+//DIMANA ADA KEMAUAN DISITU ADA JALAN
+//TETAP BERKARYA WALAU GJLS
+
+$ua = "Mozilla/5.0 (Linux; Android 5.1; HUAWEI LUA-L22) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.86 Mobile Safari/537.36";
+
+
+$Cookie = "__cfduid=ddd8474bdc91ac0fa5e9c2c1c19bed5511616045829;PHPSESSID=7df2arjg26ijbk48rrlp2bielq;spin_ref=lfj2b71ciu;bitmedia_fid=eyJmaWQiOiI1NzllZDJkNjc4ODUyNzA4NGNkNDU1NTVhMDdkZGE1YyIsImZpZG5vdWEiOiI2ODU5ZDM3N2MzZGMyNDNiZjI1M2ZiZjEzZDBmODBkOCJ9;_coinzilla_fp_=%7B%22backed%22%3A%5B%7B%22cap%22%3A8%2C%22lastCall%22%3A1616046721282%7D%5D%7D";
+
+?>
